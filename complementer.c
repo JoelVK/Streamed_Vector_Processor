de1119cc4430db416b0b43ed5a84f1d2029d1e8f
Joel Vander Klipp <joel.vanderklipp@gmail.com>
2017-02-15 12:42:16 -0500

    added forking to start processes

--- complementer.c
+++ complementer.c
@@ -6,34 +6,45 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int startProcesses();
+
 int main(){    
 
    	/* initialize varialbes */
 	int pipeFD, intLength;
     FILE* inputFD;
     char * myfifo = "/tmp/myfifo";
-	char input[1024], complement[1024], fileP[1024];
-    char * fileToken;
+	char input[1024], complement[1024], fileP[1024], fileP2[1024];
+    char * fileToken, * fileToken2;
 
     /* get input file */
+
+    printf("Enter filepath for minunend: \n");
+    scanf("%s",fileP2);
+    fileToken2 = strtok(fileP2, "\0"); 
     printf("Enter filepath for subtrahend: \n");
     scanf("%s",fileP);
+    fileToken = strtok(fileP,"\0");
+
+
     printf("Enter number of bits in each number: \n");
     scanf("%d",&intLength);
-    fileToken = strtok(fileP,"\0");
+    
     if ((inputFD = fopen(fileToken, "r")) == NULL){
         printf("file not found\n");
         exit(1);
     }   
 
+    /* begin the other two processes */
+    startProcesses(fileToken2);
     /* parse and complement data */
     int i = 0;
     while (fscanf(inputFD,"%s",input) != EOF){
         while(input[i] != '\0') {
             if(input[i] == '1'){
-                input[i] = '0';
+                complement[i] = '0';
             } else {
-                input[i] = '1';
+                complement[i] = '1';
             }
             i++;
         }
@@ -50,4 +61,36 @@ int main(){
     /* remove the FIFO */
     unlink(myfifo);
 
-    return 0;}
+    return 0;
+}
+
+int startProcesses(char * fileToken2){
+    
+    /* instantiate arguments */
+    char * argv1[2];
+    argv1[0] = "incrementer";
+    argv1[1] = NULL;
+
+    char * argv2[3];
+    argv2[0] = "adder";
+    argv2[1] = fileToken2;
+    argv2[2] = NULL;
+   
+    int pid = fork(); 
+    int pid2;
+
+    /* begin other two processes */
+    if(pid == 0) { 
+        execvp(argv1[0], argv1);
+        fprintf(stderr, "Process could not execute\n.");
+    } else {
+        pid2 = fork();
+
+        if(pid2 == 0){
+            execvp(argv2[0],argv2);
+        }
+
+                                                                                        
+    }
+    return 0;
+} 
--- incrementer.c
+++ incrementer.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
 int main (){
-    return 0;
+    
+    int pipeFD;
+    char * myfifo = "/tmp/myfifo";
+    char complement[1024], incremented[1024];
+
+
+    /* read input from pipe */
+    pipeFD = open(myfifo, O_RDONLY);
+    read(pipeFD, complement, sizeof(complement));
+    close(pipeFD);
+
+    unlink(myfifo);
+    
+    
+
+	return 0;
 }
